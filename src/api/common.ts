70c8e68c650979c59830d6944df3ea2df037d247
Nagy László Levente <nagylacika2002@gmail.com>
2023-09-06 22:09:54 +0200

    Header based api now works 2

--- src/api/common.ts
+++ src/api/common.ts
@@ -1,14 +1,14 @@
 import axios from "axios"
 
 const httpClientEun1 = axios.create({
-    baseURL: "/api", headers: {
+    baseURL: "/eun-api", headers: {
         "X-Riot-Token": ""
     } //,params: {"api_key": ""}
 })
 const httpClientEurope = axios.create({
-    baseURL: "/apii", headers: {
-        //"X-Riot-Token": ""
-    },params: {"api_key": ""}
+    baseURL: "/europe-api", headers: {
+        "X-Riot-Token": ""
+    }//,params: {"api_key": ""}
 })
 
 export {httpClientEun1, httpClientEurope}
