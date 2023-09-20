600a2091a2d6e1c3daac535c0193b959c5fc8f61
Nagy László Levente <nagylacika2002@gmail.com>
2023-09-20 17:02:05 +0200

    Added tailwind to the project

--- src/Pages/SummonerByName.tsx
+++ src/Pages/SummonerByName.tsx
@@ -1,37 +1,38 @@
 import './summoner-by-name.css'
-import SummonerData from "../components/SummonerData.tsx";
+import SummonerInput from "../components/SummonerInput.tsx";
 import {useState} from "react";
 import useSummonerData from "../hooks/useSummonerData.ts";
 import SummonerInfo from "../components/SummonerInfo.tsx";
 import LastFiveMatch from "../components/LastFiveMatches.tsx";
-import {useNavigate} from "react-router-dom";
 
 function SummonerByName() {
     const [name, setName] = useState("");
-
     const summonerData = useSummonerData(name);
-
-    const navigate = useNavigate();
-    const onMatchIDClick = () => {
+    //const navigate = useNavigate();
+    /*const onMatchIDClick = () => {
         navigate(`/match`)
-    }
-
+    }*/
     return (
-        <div className="bc">
-            <div className="all-side">
-                <div className="left-side">
-                    <SummonerData setName={setName}/>
+        <div className="w-screen">
+            <div className="inline w-3/6">
+                <div className="">
+                    <SummonerInput setName={setName}/>
                 </div>
-                <div className="right-side">
+                <div className="w-9/12 mx-auto">
                     {
                         summonerData && (
-                            <>
-                                <SummonerInfo summonerData={summonerData}/>
-                                <LastFiveMatch summonerData={summonerData}/>
-                                <div className="title">
-                                    <button onClick={onMatchIDClick}>Last game</button>
+                            <div className={"flex border-4 border-amber-400"}>
+                                <div className={"w-1/5 break-words"}>
+                                    <SummonerInfo summonerData={summonerData}/>
+                                    </div>
+                                    <div className={"w-4/6 block mx-auto"}>
+                                    <LastFiveMatch summonerData={summonerData}/>
                                 </div>
-                            </>
+                                <div className="title">
+                                    {//<button onClick={onMatchIDClick}>Last game</button>
+                                    }
+                                </div>
+                            </div>
                         )
                     }
                 </div>
